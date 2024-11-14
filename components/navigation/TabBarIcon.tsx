1dc8450e74ca0bbecb1fb85ad15f8a1deabb2779
ak033 <59183564+ak033@users.noreply.github.com>
2024-11-13 19:25:44 -0700

    Update TabBarIcon.tsx

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,19 +1,4 @@
-// // components/navigation/TabBarIcon.tsx
-// import React from 'react';
-// import { Ionicons } from '@expo/vector-icons';
 
-// type IoniconName = keyof typeof Ionicons.glyphMap;
-
-// interface TabBarIconProps {
-//   name: IoniconName;
-//   color: string;
-// }
-
-// export const TabBarIcon: React.FC<TabBarIconProps> = ({ name, color }) => {
-//   return <Ionicons name={name} size={24} color={color} />;
-// };
-
-// components/navigation/TabBarIcon.tsx
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
